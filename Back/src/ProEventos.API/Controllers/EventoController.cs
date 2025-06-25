bbf9438a5d4ad401f005cf0d1c706a28394d1ff8
Pedro Augusto <pcarreteiro01@gmail.com>
2025-06-25 17:27:01 -0300

    Primeira API Get

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[] { //Enumerar por evento 
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular e .NET Core",
+                    Local = "Belo Horizonte",
+                    Lote = "1°Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular e Suas Novidades",
+                    Local = "São Paulo",
+                    Lote = "2°Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto1.png"
+                }
+            };
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<EventoController> _logger;
+
+        public EventoController()
+        {
+ 
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {   
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {   
+            return _evento.Where(evento => evento.EventoId == id);       
+        }
+
+        [HttpPost]
+        public string Post()
+        {   
+            return "valuePost";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {   
+            return $"valuePut com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {   
+            return $"valueDelete com id = {id}";
+        }
+    }
+}
